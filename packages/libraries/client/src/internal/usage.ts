33d06f2197d2d17be1c8e1c8921cbe772909a958
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2023-02-02 10:27:14 -0500

    chore: prettier

--- packages/libraries/client/src/internal/usage.ts
+++ packages/libraries/client/src/internal/usage.ts
@@ -371,7 +371,7 @@ export function createCollector({
           return {
             ...node,
             arguments: [],
-          }
+          };
         },
         Argument(node) {
           const parent = typeInfo.getParentType()!;
